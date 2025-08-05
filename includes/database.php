03fb1d82f14aa4300bb26c4c8ea6674e98c83c61
andresjimenez11 <njimenezgiraldo@gmail.com>
2025-08-05 17:15:45 -0500

    first commit

---
+++ includes/database.php
@@ -0,0 +1,15 @@
+<?php
+
+$db_host = $_ENV['DB_HOST'];
+$db_user = $_ENV['DB_USER'];
+$db_pass = $_ENV['DB_PASS'];
+$db_name = $_ENV['DB_NAME'];
+
+$db = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if (!$db) {
+    echo "Error: No se pudo conectar a MySQL.";
+    echo "errno de depuración: " . mysqli_connect_errno();
+    echo "error de depuración: " . mysqli_connect_error();
+    exit;
+}
