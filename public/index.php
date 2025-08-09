05e89fab7a4d599ef8cff9a1c02bb1ac11e23172
andresjimenez11 <njimenezgiraldo@gmail.com>
2025-08-09 14:42:44 -0500

    feat: first steps routing

--- public/index.php
+++ public/index.php
@@ -2,10 +2,30 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\LoginController;
 use MVC\Router;
 $router = new Router();
 
+// Login
+$router->get('/', [LoginController::class, 'login']);
+$router->post('/', [LoginController::class, 'login']);
+$router->get('/logout', [LoginController::class, 'logout']);
 
+// Crear cuenta
+$router->get('/crear', [LoginController::class, 'create']);
+$router->post('/crear', [LoginController::class, 'create']);
+
+// Olvide mi password
+$router->get('/olvide', [LoginController::class, 'forget']);
+$router->post('/olvide', [LoginController::class, 'forget']);
+
+// Confirmación nuevo password
+$router->get('/reestablecer', [LoginController::class, 'reset']);
+$router->post('/reestablecer', [LoginController::class, 'reset']);
+
+// Confirmación de cuenta
+$router->get('/mensaje', [LoginController::class, 'message']);
+$router->get('/confirmar', [LoginController::class, 'confirm']);
 
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
